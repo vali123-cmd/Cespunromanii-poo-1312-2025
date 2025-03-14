2688ce5fd1768616a3d409afbb813aff47dc89d4
Danciu Valentin-Nicolae <valentindanciu02@gmail.com>
2025-03-14 17:48:00 +0200

    fixed json, added print operator for every class.

--- main.cpp
+++ main.cpp
@@ -6,27 +6,16 @@
 #include <nlohmann/json.hpp>
 #include <random>
 #include <cmath>
+
 #include <queue>
 
 
 using json = nlohmann::json;
-
-
-class Cabral {
-    std::vector<std::string> sayings;
-    public:
-    explicit Cabral(const std::vector<std::string>& sayings_) {
-        sayings = sayings_;
-    }
-
-
-};
-
-
 class Player {
     int m_score=0;
     std::string m_name;
     std::string m_family;
+
 public:
     void increaseScore(int& m_score, int points_given, int bonus_multiplier) {
         m_score+= points_given*bonus_multiplier;
@@ -37,32 +26,41 @@ public:
         return m_score;
     }
 
+
+
     Player(const std::string& name, const std::string& family) {
 
         m_name = name;
         m_family = family;
         std::cout<<"Jucatorul "<<m_name<<"din familia "<<m_family<<"a fost initializat cu succes."<<'\n';
     }
-
+    friend std::ostream& operator<<(std::ostream& os, const Player& p);
 
 };
+std::ostream& operator<<(std::ostream& os, const Player& p ) {
+
+    std::cout<<"Nume jucator: "<<p.m_name<<" din familia"<< p.m_family<<'\n';
+    return os;
+
+}
 class Family{
     std::string family_name;
     int family_score;
     std::vector<Player> players;
-    void calculate_score(const std::vector<Player>& players) {
-        for (const auto& player : players) {
-            family_score+=player.m_score1();
-        }
-    }
-
 public:
+    const int calculate_score()  {
+        int score = 0;
+        for (const auto& player : players) {
+            score += player.m_score1();
+        }
+        return score;
+    }
     [[nodiscard]] std::string family_name1() const {
         return family_name;
     }
 
-    [[nodiscard]] int family_score1()  {
-        calculate_score(players);
+    [[nodiscard]] int family_score1() const {
+
         return family_score;
     }
 
@@ -73,6 +71,11 @@ public:
     }
 
 };
+std::ostream& operator<<(std::ostream& os,  Family& family) {
+
+    os<<family.family_name1()<<": "<<family.calculate_score()<<'\n';
+    return os;
+}
 
 class Question {
     std::string m_text;
@@ -80,12 +83,23 @@ class Question {
 
 
 public:
-    explicit Question(const std::string& text_) {
+    Question(const std::string& text_, const std::vector<std::pair<std::string, int>>& answers_) {
         m_text = text_;
+        answers = answers_;
     }
 
-};
+    [[nodiscard]] std::string m_text1() const {
+        return m_text;
+    }
 
+    [[nodiscard]] std::vector<std::pair<std::string, int>> answers1() const {
+        return answers;
+    }
+};
+std::ostream& operator<<(std::ostream& os, const Question& q) {
+    os<<q.m_text1()<<'\n';
+    return os;
+}
 
 
 class Round {
@@ -93,10 +107,10 @@ class Round {
     int round_id = 1;
     json data;
 
-    std::string pickRandQuestion() {
+    static int pickRandIndex(const json &data) {
         //Functie care alege o intrebare random din json.
         if (data["intrebari"].empty()) {
-            return "No questions available.";
+            return -1;
         }
 
         std::random_device rd;
@@ -104,14 +118,31 @@ class Round {
         std::uniform_int_distribution<> distr(0, data["intrebari"].size() - 1);
 
         int randomIndex = distr(gen);
-        return data["intrebari"][randomIndex]["intrebare"];
+        return randomIndex;
     }
 public:
+    [[nodiscard]] int round_id1() const {
+        return round_id;
+    }
+
     Round(int round_id_, const json &data_){
         round_id = round_id_;
         data = data_;
         std::cout<<"Runda "<<round_id<<" a inceput"<<'\n';
-
+        int randindex = pickRandIndex(data_);
+        if (randindex == -1) {
+            std::cerr<<"Nu am putut gasi intrebari in json!"<<'\n';
+            //NOTA: De inchis fereastra atunci cand se intampla asta.
+        }
+        else {
+            std::vector<std::pair<std::string, int>> answers;
+            for (const auto& item : data_["intrebari"][randindex]["raspunsuri"]) {
+                answers.push_back(std::make_pair(item["raspuns"], item["punctaj"]));
+            }
+            Question q = Question(data["intrebari"][randindex]["intrebare"],
+                answers);
+            std::cout<<q;
+        }
 
 
 
@@ -123,6 +154,10 @@ public:
      * pe baza acestei runde!' */
 
 };
+std::ostream& operator<<(std::ostream& os, const Round& q) {
+    os<<q.round_id1()<<'\n';
+    return os;
+}
 
 
 class Game {
@@ -151,7 +186,7 @@ class Game {
         }
     }
     void parseJson() {
-    std::ifstream file("source/data.json");
+        std::ifstream file("../source/data.json");
     if (!file.is_open()) {
         std::cerr<<"Nu am putut deschide fisierul json!"<<'\n';
         //NOTA: De inchis fereastra atunci cand se intampla asta.
@@ -164,6 +199,7 @@ class Game {
 
     public:
     Game() {
+        parseJson();
         std::string family1;
         std::string family2;
         std::cout << "Salut! Bine ai venit la Family feud/(Ce spun romanii?)! Eu sunt gazda emisiunii, Cabral./"
@@ -178,21 +214,23 @@ class Game {
         getPlayers(players2, family2);
         Family secondFam(family2, 0, players2);
 
+        for (int i=1;i<=6;i++) {
+
+            Round round(i, data);
+        }
     }
 };
 
+std::ostream& operator<<(std::ostream& os, const Game &g) {
+    std::cout<<"Game currently initiated and running. Enjoy! -From Cabral himself."<<'\n';
+    return os;
+}
 
 
 
 int main() {
 
+    Game main;
 
     return 0;
-
-
-
-
-
-
-
 }
