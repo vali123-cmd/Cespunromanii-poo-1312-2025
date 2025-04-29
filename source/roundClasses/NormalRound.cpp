eac07a4910f9e4c2e24c759a2466c9ebedff3f9e
Danciu Valentin-Nicolae <Danciu Valentin-Nicolae>
2025-04-29 17:22:50 +0300

    fixed some warnings4

---
+++ source/roundClasses/NormalRound.cpp
@@ -0,0 +1,5 @@
+//
+// Created by danciu-valentin-nicolae on 4/29/25.
+//
+
+#include "NormalRound.h"
---
+++ source/roundClasses/NormalRound.h
@@ -0,0 +1,16 @@
+//
+// Created by danciu-valentin-nicolae on 4/29/25.
+//
+
+#ifndef NORMALROUND_H
+#define NORMALROUND_H
+
+
+
+class NormalRound {
+
+};
+
+
+
+#endif //NORMALROUND_H
