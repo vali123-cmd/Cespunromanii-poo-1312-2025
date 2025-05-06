7472894d2ee54d8b1b2602ce1f9a24ce1b71c815
danciu-valentin-nicolae <valentindanciu02@gmail.com>
2025-05-06 11:47:17 +0300

    added logo

---
+++ source/ClipArts.h
@@ -0,0 +1,17 @@
+//
+// Created by danciu-valentin-nicolae on 06.05.2025.
+//
+
+#ifndef CLIPARTS_H
+#define CLIPARTS_H
+
+
+
+class ClipArts {
+public:
+    void printLogo();
+};
+
+
+
+#endif //CLIPARTS_H
--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 
+#include "ClipArts.h"
 #include "gameClasses/Game.h"
 #include "gameClasses/NormalGame.h"
 #include "gameClasses/SpecialGame.h"
@@ -8,6 +9,8 @@
 
 int main() {
     std::ifstream file("intrebari.json");
+    ClipArts art;
+    art.printLogo();
 
     if (!file.is_open()) {
         return 0;
